3b89b6126241f95c09fde0fb5488878c18557396
Ahmet Selman Dur <ahmetselmandur72@gmail.com>
2025-02-07 18:49:18 +0300

    profile sayfası güncellendi isim eklendi

--- Uygulama/src/ProfileSayfa.java
+++ Uygulama/src/ProfileSayfa.java
@@ -1,14 +1,21 @@
-﻿import javax.swing.*;
+import javax.swing.*;
+import java.awt.*;
 
 public class ProfileSayfa {
     JFrame frame = new JFrame();
+    JLabel label = new JLabel();
 
     public ProfileSayfa()
     {
+        label.setText("İsim: Ahmet Selman Dur");
+        label.setFont(new Font(null,Font.PLAIN,25));
+
+        frame.add(label);
+
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Profile Sayfa");
-        frame.setLayout(null);
+        frame.setLayout(new GridLayout(3,1));
         frame.setVisible(true);
     }
 }
